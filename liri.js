167b97ff764de5bc60fe9e322fc0abada7be9b71
Steve Chronister <stephen.g.chronister@gmail.com>
2016-11-29 11:56:41 -0600

    added package.json and initial liri.js app structure

---
+++ liri.js
@@ -0,0 +1,36 @@
+var request = require("request");
+var twitter = require("twitter");
+var spotify = require("spotify");
+var keys = require("./keys.js");
+
+//saving the twitter authentication info:
+var client = new Twitter({
+  consumer_key: keys.consumer_key,
+  consumer_secret: keys.consumer_secret,
+  access_token_key: keys.access_token_key,
+  access_token_secret: keys.access_token_secret
+});
+
+//get the command line arguments
+var argArray = []
+for (var i = 2; i < process.argv.length; i++){
+	argArray.push(process.argv[i]);
+};
+
+//switch statement for the first argument
+switch (argArray[0]){
+	case "my-tweets":
+		client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response) {
+		   console.log(tweets);
+		});
+		break;
+	case "spotify-this-song":
+
+		break;
+	case "movie-this":
+
+		break;
+	case "do-what-it-says":
+
+		break;
+}
